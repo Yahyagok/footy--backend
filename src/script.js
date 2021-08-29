506c79b856396189d3b126acd4185ada17bab49a
Yahyagok <ygok17@hotmail.com>
2021-08-29 16:48:52 -0400

    some little changes

--- src/script.js
+++ src/script.js
@@ -153,8 +153,8 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(player =>  {
               eachPlayer.innerHTML = `
               <h5>Player Name: ${player.name} </h5>
-              <h5>Player Number:   ${player.number}</h5>
-              <h5> Player Type:   ${player.kind}</h5>
+              <h5>Player Number:${player.number}</h5>
+              <h5> Player Type:${player.kind}</h5>
               <h5>Player Country:  ${player.country}<h5>
               <h5>Player Age:   ${player.age} <h5>
                `
